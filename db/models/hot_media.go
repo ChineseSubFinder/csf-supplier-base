64b4b0be63c8bd7d73348a1f4eabdfda5a752550
allan716 <525223688@qq.com>
2023-02-09 10:36:26 +0800

    还原 primary_key 改动
    Signed-off-by: allan716 <525223688@qq.com>

--- db/models/hot_media.go
+++ db/models/hot_media.go
@@ -1,7 +1,7 @@
 package models
 
 type HotMedia struct {
-	IMDBid           string `gorm:"column:imdb_id;unique;type:varchar(20);not null"`
+	IMDBid           string `gorm:"column:imdb_id;unique;primary_key;type:varchar(20);not null"`
 	Count            int    `gorm:"column:count;type:int;not null"`                 // 从热门列表中更新的次数
 	LastCrawlingTime int64  `gorm:"column:last_crawling_time;type:bigint;not null"` // 上次爬取的时间
 	IsHotToday       bool   `gorm:"column:is_hot_today;type:tinyint(1);not null"`   // 是否是今天的热门
