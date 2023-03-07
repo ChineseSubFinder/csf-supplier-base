a2f6c7765cab34690060e7417a9b6fa8ec9de716
allan716 <525223688@qq.com>
2023-03-07 15:39:07 +0800

    测试代码
    Signed-off-by: allan716 <525223688@qq.com>

--- pkg/captcha_client/captcha_client_test.go
+++ pkg/captcha_client/captcha_client_test.go
@@ -2,13 +2,17 @@ package captcha_client
 
 import (
 	"github.com/ChineseSubFinder/csf-supplier-base/pkg/settings"
+	"github.com/allanpk716/rod_helper"
 	"testing"
 )
 
 func TestCaptchaClient_Get(t *testing.T) {
 
+	rod_helper.InitFakeUA(settings.Get().CacheRootDirPath, settings.Get().TMDBConfig.TMDBHttpProxy)
 	ca := NewCaptchaClient(settings.Get().CaptchaConfig.ZiMuKu.Url)
-	result, err := ca.GetFromImgFile("C:\\WorkSpace\\TrainData\\zimuku\\Validate_mark\\03055_1b3e6b439e2645f4bf02f59eb388906e.bmp")
+	dd := ""
+	result, err := ca.GetFromImgBase64(dd)
+	//result, err := ca.GetFromImgFile("123.bmp")
 	if err != nil {
 		t.Fatal(err)
 	}
