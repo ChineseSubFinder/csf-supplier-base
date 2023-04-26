484956710adb68fa31a7ab565eee20f98001325a
allan716 <525223688@qq.com>
2023-04-26 15:13:28 +0800

    新增整个季度的字段
    Signed-off-by: allan716 <525223688@qq.com>

--- db/models/user_share_subtitle_tv.go
+++ db/models/user_share_subtitle_tv.go
@@ -2,6 +2,7 @@ package models
 
 type UserShareSubtitleTV struct {
 	UserShareSubtitleInfo
-	Season  int `gorm:"column:season;type:int;index;not null"`  // 如果无法识别就是 -1
-	Episode int `gorm:"column:episode;type:int;index;not null"` // 如果无法识别就是 -1
+	FullSeasonSha256 string `gorm:"column:full_season_sha256;type:char(64);index"` // 一整季的 SHA256，因为上传的时候是分散的文件，这里计算会比较特别，具体看详细的实现
+	Season           int    `gorm:"column:season;type:int;index;not null"`         // 如果无法识别就是 -1
+	Episode          int    `gorm:"column:episode;type:int;index;not null"`        // 如果无法识别就是 -1
 }
