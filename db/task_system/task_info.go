a60385fcf82e5760f55c68905c41a927664eb0ac
allan716 <525223688@qq.com>
2023-05-13 18:42:45 +0800

    保存进度
    Signed-off-by: allan716 <525223688@qq.com>

--- db/task_system/task_info.go
+++ db/task_system/task_info.go
@@ -17,12 +17,6 @@ type TaskInfo struct {
 	FinishDataRPath string   `gorm:"column:finish_data_r_path;type:varchar(255);not null"`  // 这个任务完成后，存储的数据的相对路径，相对于 R2 存储
 	FinishDataSize  int      `gorm:"column:finish_data_size;type:int;not null"`             // 这个任务完成后，存储的数据的大小，单位：字节
 
-	NeedSplit bool `gorm:"column:need_split;type:tinyint(1);not null;default:0"` // 是否需要拆分的任务，比如字幕的翻译，就需要分解成多个任务
-	SplitDone bool `gorm:"column:split_done;type:tinyint(1);not null;default:0"` // 是否已经拆分完成
-
-	NeedMerge bool `gorm:"column:need_merge;type:tinyint(1);not null;default:0"` // 是否需要合并的任务，比如字幕的翻译，就需要合并成一个任务
-	MergeDone bool `gorm:"column:merge_done;type:tinyint(1);not null;default:0"` // 是否已经合并完成
-
 	DataVersion string `gorm:"column:data_version;type:varchar(255);not null"` // 任务数据的版本
 
 	SrcDataFileUrl    string `gorm:"-" json:"src_data_file_url"`    // 源任务数据的下载 URL
